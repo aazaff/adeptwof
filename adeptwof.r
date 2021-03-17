d45e716abc9e0382b6a9ec30145c7d7be2404aca
azaffos <azaffos@zaffosmac.local>
2021-03-17 15:46:10 -0700

    dramatically improved the upload efficiency

--- adeptwof.r
+++ adeptwof.r
@@ -33,7 +33,7 @@ if (suppressWarnings(require("pbapply"))==FALSE) {
 # from postgis entirely into R, but the benefit of doing it in postgis is because that allows me to preserve
 # a stable database rather than recreating data constantly 
 Driver<-dbDriver("PostgreSQL") # Establish database driver
-Ecos<-dbConnect(Driver, dbname = "mapzen", host = "localhost", port = 5432, user = "zaffos")
+Mapzen<-dbConnect(Driver, dbname = "mapzen", host = "localhost", port = 5432, user = "zaffos")
 
 # Change the maximum timeout t0 300 second. This will allow you to download larger datafiles from 
 # the paleobiology database.
@@ -51,15 +51,29 @@ options(timeout=600)
 ########################################### DOWNLOAD WOF DATASE, GET ########################################
 #############################################################################################################
 # A simple function for reading in the geojson
-readGeojson = function(path=Metadata$path,header) {
-    File = paste0(header,path)
-    Geometry = st_read(File,quiet=TRUE)
-    Geometry = st_as_text(st_geometry(Geometry))
-    return(Geometry)
+# Hardcodes a BUNCH of stuff, most notably the database connection parameter
+readWOF = function(Path) {
+    Initial = sf::st_read(Path,quiet=TRUE)
+    if ("wof.hierarchy"%in%names(Initial)!=TRUE) {return(NA)}
+    id = Initial$wof.id
+    name = Initial$wof.name
+    placetype = Initial$wof.placetype
+    macroregion = jsonlite::fromJSON(Initial$wof.hierarchy)$macroregion_id
+    region = jsonlite::fromJSON(Initial$wof.hierarchy)$region_id
+    macrocounty = jsonlite::fromJSON(Initial$wof.hierarchy)$macrocounty_id
+    county = jsonlite::fromJSON(Initial$wof.hierarchy)$county_id
+    localadmin = jsonlite::fromJSON(Initial$wof.hierarchy)$localadmin_id
+    locality = jsonlite::fromJSON(Initial$wof.hierarchy)$locality_id
+    borough = jsonlite::fromJSON(Initial$wof.hierarchy)$borough_id
+    neighborhood = jsonlite::fromJSON(Initial$wof.hierarchy)$neighborhood_id
+    geom = unlist(st_as_text(Initial$geometry))
+    Flattened = as.data.frame(cbind(id,name,placetype,macroregion,region,macrocounty,county,localadmin,locality,borough,neighborhood,geom),stringsAsFactors=FALSE)  
+    # Write them to the WOF table
+    sf::st_write(dsn=Mapzen,obj=st_as_sf(Flattened,wkt="geom"),"usa_wof",append=TRUE,row.names=FALSE)
+    return(id)
     }
 
 ########################################### DOWNLOAD WOF DATASE, GET ########################################
-
 # Download the US WOF database as a TAR bundle, could do this through R, but I chose not to!
 # https://data.geocode.earth/wof/dist/bundles/whosonfirst-data-admin-us-latest.tar.bz2
 # Can also be found at the following github repo https://github.com/whosonfirst-data/whosonfirst-data-admin-us
@@ -68,13 +82,17 @@ readGeojson = function(path=Metadata$path,header) {
 # download.file("https://data.geocode.earth/wof/dist/bundles/whosonfirst-data-admin-us-latest.tar.bz2",destfile="~/Downloads")
 # untar("...")
 
-# Get a list of the files in metadata folder
-# Metadata = list.files(path="~/Downloads/whosonfirst-data-admin-us-latest/meta",pattern="*.csv",full.names=TRUE)
-# Read them into R and then collapse
-# Metadata = do.call(rbind,lapply(Metadata,read.csv))
-# Clean it up by removing features without proper names (mostly from some assholes called quattroshapes?)
-# Metadata = subset(Metadata,Metadata[,"name"]!="")
+# Create the blank table
+dbSendQuery(Mapzen,"CREATE TABLE usa_wof (id bigint PRIMARY KEY,name varchar, placetype varchar, macroregion bigint, region bigint, macrocount bigint, county bigint, localadmin bigint, locality bigint, borough bigint, neighborhood bigint, geom geometry)")
 
-# Get the geometries
-# geom = sapply(Metadata$path,readGeojson,"~/Downloads/whosonfirst-data-admin-us-latest/data/")
-# Metadata = cbind(Metadata,geom)
+# Get a list of the geojsons
+Metadata = list.files(path="~/Downloads/whosonfirst-data-admin-us-latest/data",pattern="*.geojson",full.names=TRUE,recursive=TRUE)
+
+# Write them into the postgres database
+# Definitely parallelize this if you ever do it again... the i/o is ridic
+Write = pbsapply(Metadata,readWOF)
+
+# Gotta make a god damn index and set a primary key for the love of god
+dbSendQuery(Mapzen,"ALTER TABLE usa_wof ADD PRIMARY KEY (id);")
+dbSendQuery(Ecos,"CREATE INDEX ON usa_wof USING GiST (geom);") # probably not needed since it is point data
+dbSendQuery(Mapzen,"VACUUM ANALYZE usa_wof;")
